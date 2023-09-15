3f4bda0ec0ededb23a0c1962851cea59140dcf1c
22ermiller <22ermiller@gmail.com>
2023-09-15 15:06:44 -0600

    made predictions

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,2 +1,48 @@
 library(tidyverse)
+library(vroom)
+library(tidymodels)
+
+## Load in Data
+
+bike_train <- vroom("./train.csv") %>%
+  select(-casual, -registered)
+
+bike_test <- vroom("./test.csv")
+
+## Prep Recipe
+
+my_recipe <- recipe(count~., data = bike_train) %>%
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>%
+  step_mutate(weather = as.factor(weather),
+              season = as.factor(season),
+              workingday = as.factor(workingday),
+              holiday = as.factor(holiday)) %>%
+  step_time(datetime, features= c("hour"))
+
+prepped_recipe <- prep(my_recipe)
+bake(prepped_recipe, bike_test)
+
+
+# Set up Linear Regression ------------------------------------------------
+
+
+my_mod <- linear_reg() %>%
+  set_engine("lm") # linear regression model
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>% 
+  add_model(my_mod) %>%
+  fit(data = bike_train)
+
+# Make predictions
+
+bike_predictions <- predict(bike_workflow,
+                            new_data =bike_test) %>%
+  mutate(preds = ifelse(.pred < 0, 0, .pred))
+
+final_predictions <- tibble(datetime = bike_test$datetime, count = bike_predictions$preds)
+
+final_predictions$datetime <- as.character(format(final_predictions$datetime))
+
+vroom_write(final_predictions, "final_predictions.csv", delim = ",")
 
